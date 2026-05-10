60da2779e1804d6c43885f67791cd8390de416ba
JuanAndresCano <juandroid500@gmail.com>
2026-05-10 14:34:15 -0500

    fix: Connection to postgres fix

--- services/circleguard-auth-service/src/test/java/com/circleguard/auth/integration/LoginIntegrationTest.java
+++ services/circleguard-auth-service/src/test/java/com/circleguard/auth/integration/LoginIntegrationTest.java
@@ -14,12 +14,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+
+import java.time.Duration;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
